68a9cab6296e54a34b39e6a5886f4b8bfd7cfbc4
Filipe Pereira <filipe.bass93@gmail.com>
2020-08-24 11:37:38 -0300

    Atualizando banco sempre em modo DEV

--- sqlite/database/ConnectionBase.js
+++ sqlite/database/ConnectionBase.js
@@ -2,11 +2,32 @@ import { Asset } from 'expo-asset';
 import * as FileSystem from 'expo-file-system';
 import * as SQLite from 'expo-sqlite';
 
+const makeid = (length) => {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+ }
+
 export const retrieveDatabase = async () => {
 
     return await (new Promise(async (resolve, reject) => {
+
+        let databaseName;
+
+        if (__DEV__){
+            databaseName = `database_${makeid(15)}.db`;
+        } else {
+            databaseName = 'produtos.db';
+        }
+
         const uri = Asset.fromModule(require('../assets/produtos.db')).uri
-        const toDownload = `${FileSystem.documentDirectory}/produtos.db`
+        const toDownload = `${FileSystem.documentDirectory}/${databaseName}`
 
         const info = await FileSystem.getInfoAsync(toDownload)
 
@@ -18,12 +39,12 @@ export const retrieveDatabase = async () => {
             })
 
             if (result){
-                resolve({ success: true, db: SQLite.openDatabase('../produtos.db') })
+                resolve({ success: true, db: SQLite.openDatabase(`../${databaseName}`) })
             } else {
                 resolve({ success: false, db: null})
             }
         }  else {
-            resolve({ success: true, db: SQLite.openDatabase('../produtos.db') })
+            resolve({ success: true, db: SQLite.openDatabase(`../${databaseName}`) })
         }
     }))
 
